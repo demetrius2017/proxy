bff72da34d2f694222385a171696052bb2ae5498
Dmitrii MacPro <demetrius_i@mail.ru>
2024-08-24 16:29:33 +0900

    refactor: Update Dockerfile and main.py for Telegram bot deployment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import logging
-from telegram import Update, WebAppInfo
+from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-from flask import Flask, send_file
-import threading
 
 # Замените на свой токен
 TOKEN = "6740830002:AAFcw7PqsWgGp4cJna24vofPtU1P2dCT4yE"
@@ -13,13 +11,6 @@ WEBAPP_URL = "http://94.241.174.212"
 # Настройка логирования
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-# Инициализация Flask приложения
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return send_file('index.html')
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Отправляет приветственное сообщение с кнопкой для запуска Web App."""
@@ -32,20 +23,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         }
     )
 
-def run_flask():
-    app.run(host="0.0.0.0", port=8080)
 
 def main() -> None:
     """Запускает бота и веб-сервер."""
-    # Запуск Flask в отдельном потоке
-    run_flask()
-    flask_thread = threading.Thread(target=run_flask)
-    # flask_thread.start()
 
-    # # Настройка и запуск Telegram бота
-    # application = Application.builder().token(TOKEN).build()
-    # application.add_handler(CommandHandler("start", start))
-    # application.run_polling()
+    # Настройка и запуск Telegram бота
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.run_polling()
 
 if __name__ == "__main__":
     main()
